6d5e6e77a1b193402162f522f3ff162c9235edf7
Statyk7 <Statyk7@users.noreply.github.com>
2010-09-10 11:14:15 +0000

    Minor fix to support Windows 7 Starter Edition. Version changed to 1.4.0910.3.

--- src/Log2Console/Properties/AssemblyInfo.cs
+++ src/Log2Console/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.0910.2")]
-[assembly: AssemblyFileVersion("1.4.0910.2")]
+[assembly: AssemblyVersion("1.4.0910.3")]
+[assembly: AssemblyFileVersion("1.4.0910.3")]
