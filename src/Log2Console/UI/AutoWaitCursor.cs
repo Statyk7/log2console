93814fadc21969469ae87b0585dbed8d980021b1
LexLavnikov_cp <SND\LexLavnikov_cp>
2010-10-03 16:41:25 +0000

    Fixed crash on start on Windows 7x64

--- src/Log2Console/UI/AutoWaitCursor.cs
+++ src/Log2Console/UI/AutoWaitCursor.cs
@@ -4,38 +4,39 @@ using System.Windows.Forms;
 
 namespace Log2Console.UI
 {
-	/// <summary>
-	/// Auto Wait Cursor utility class.
-	/// Usage:
-	/// using (new AutoWaitCursor())
-	/// { ...long task... }
-	/// </summary>
-	public sealed class AutoWaitCursor : IDisposable
-	{
-		public AutoWaitCursor()
-		{
-			Enabled = true;
-		}
+  /// <summary>
+  /// Auto Wait Cursor utility class.
+  /// Usage:
+  /// using (new AutoWaitCursor())
+  /// { ...long task... }
+  /// </summary>
+  public sealed class AutoWaitCursor : IDisposable
+  {
+    public AutoWaitCursor()
+    {
+      Enabled = true;
+    }
 
-		public void Dispose()
-		{
-			Enabled = false;
-		}
+    public void Dispose()
+    {
+      Enabled = false;
+    }
 
-		public static bool Enabled
-		{
-			get { return Application.UseWaitCursor; }
-			set
-			{
-				if (value == Application.UseWaitCursor) return;
-				Application.UseWaitCursor = value;
-				Form f = Form.ActiveForm;
-				if (f != null && f.Handle != null)   // Send WM_SETCURSOR
-					SendMessage(f.Handle, 0x20, f.Handle, (IntPtr)1);
-			}
-		}
+    public static bool Enabled
+    {
+      get { return Application.UseWaitCursor; }
+      set
+      {
+        if (value == Application.UseWaitCursor) return;
+        Application.UseWaitCursor = value;
+        
+        var f = Form.ActiveForm;
+        if (f != null && f.Visible && f.Handle != IntPtr.Zero)   // Send WM_SETCURSOR
+          SendMessage(f.Handle, 0x20, f.Handle, (IntPtr)1);
+      }
+    }
 
-		[System.Runtime.InteropServices.DllImport("user32.dll")]
-		private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
-	}
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+  }
 }
