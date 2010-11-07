172eabea24867fa33454520b96ad287944dca3cf
Statyk7_cp <SND\Statyk7_cp>
2010-11-07 19:27:48 +0000

    Changed to version 1.5.1110.1.

--- src/Log2Console/Properties/AssemblyInfo.cs
+++ src/Log2Console/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.1010.1")]
-[assembly: AssemblyFileVersion("1.5.1010.1")]
+[assembly: AssemblyVersion("1.5.1110.1")]
+[assembly: AssemblyFileVersion("1.5.1110.1")]
